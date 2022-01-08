6f8259cb76abadb7596d860e4ca8265a2771ff69
Milind Sai Waghrey <milindsw6@gmail.com>
2022-01-07 20:01:03 -0500

    Updates

---
+++ precache-manifest.af50d0750f039e53db800b1fc9cf7bc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f134c1f174e353bf60d1",
+    "url": "/spacestagram/static/css/main.9ffd3621.chunk.css"
+  },
+  {
+    "revision": "f134c1f174e353bf60d1",
+    "url": "/spacestagram/static/js/main.f134c1f1.chunk.js"
+  },
+  {
+    "revision": "2a6b1802e9b6271fb83f",
+    "url": "/spacestagram/static/js/1.2a6b1802.chunk.js"
+  },
+  {
+    "revision": "03f349f42871e0e8d516",
+    "url": "/spacestagram/static/css/2.a3763209.chunk.css"
+  },
+  {
+    "revision": "03f349f42871e0e8d516",
+    "url": "/spacestagram/static/js/2.03f349f4.chunk.js"
+  },
+  {
+    "revision": "33f00a3614847fe0a967",
+    "url": "/spacestagram/static/js/runtime~main.33f00a36.js"
+  },
+  {
+    "revision": "32b5bbcb2aae1d274a22fbd1604c094d",
+    "url": "/spacestagram/static/media/gray.32b5bbcb.png"
+  },
+  {
+    "revision": "5cadf1baa6451e03dba583fd8075437b",
+    "url": "/spacestagram/index.html"
+  }
+];
